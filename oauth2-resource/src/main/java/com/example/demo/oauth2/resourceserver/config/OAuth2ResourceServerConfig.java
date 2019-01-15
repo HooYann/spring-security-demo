95ac4778d77682325aa08c0a80b27707914f06a9
Yann <huyy2b@sina.com>
2019-01-15 21:23:12 +0800

    1

--- oauth2-resource/src/main/java/com/example/demo/oauth2/resourceserver/config/OAuth2ResourceServerConfig.java
+++ oauth2-resource/src/main/java/com/example/demo/oauth2/resourceserver/config/OAuth2ResourceServerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.jwt.crypto.sign.RsaVerifier;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -21,6 +22,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableResourceServer
 public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private static final String RESOURCE_ID_TEST = "resource_id_test";
+
     @Override
     public void configure(HttpSecurity http) throws  Exception {
         http
@@ -32,7 +36,7 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resourceConfig) throws Exception {
-        resourceConfig.resourceId("dd").stateless(true);
+        resourceConfig.resourceId(RESOURCE_ID_TEST).stateless(true);
         //resourceConfig.tokenExtractor();
         resourceConfig.tokenStore(tokenStore());
     }
@@ -45,13 +49,17 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("9527");
+        //密钥
+        //converter.setSigningKey("9527");
+        //公钥
+        //converter.setVerifier(new RsaVerifier("-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjvBtqsGCOmnYzwe/+HvgOqlKk6HPiLEzS6uCCcnVkFXrhnkPMZ+uQXTR0u+7ZklF0XC7+AMW8FQDOJS1T7IyJpCyeU4lS8RIf/Z8RX51gPGnQWkRvNw61RfiSuSA45LR5NrFTAAGoXUca/lZnbqnl0td+6hBDVeHYkkpAsSck1NPhlcsn+Pvc2Vleui/Iy1U2mzZCM1Vx6Dy7x9IeP/rTNtDhULDMFbB/JYs+Dg6Zd5Ounb3mP57tBGhLYN7zJkN1AAaBYkElsc4GUsGsUWKqgteQSXZorpf6HdSJsQMZBDd7xG8zDDJ28hGjJSgWBndRGSzQEYU09Xbtzk+8khPuwIDAQAB\n-----END PUBLIC KEY-----"));
+        converter.setVerifierKey("-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjvBtqsGCOmnYzwe/+HvgOqlKk6HPiLEzS6uCCcnVkFXrhnkPMZ+uQXTR0u+7ZklF0XC7+AMW8FQDOJS1T7IyJpCyeU4lS8RIf/Z8RX51gPGnQWkRvNw61RfiSuSA45LR5NrFTAAGoXUca/lZnbqnl0td+6hBDVeHYkkpAsSck1NPhlcsn+Pvc2Vleui/Iy1U2mzZCM1Vx6Dy7x9IeP/rTNtDhULDMFbB/JYs+Dg6Zd5Ounb3mP57tBGhLYN7zJkN1AAaBYkElsc4GUsGsUWKqgteQSXZorpf6HdSJsQMZBDd7xG8zDDJ28hGjJSgWBndRGSzQEYU09Xbtzk+8khPuwIDAQAB\n-----END PUBLIC KEY-----");
         return converter;
     }
 
 
-    @Bean
+    /*@Bean
     public JwtDecoder jwtDecoder() {
         return new NimbusJwtDecoderJwkSupport("http://localhost:8087/.well-known/jwks.json");
-    }
+    }*/
 }
