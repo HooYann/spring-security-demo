e210cbe80d5b757d61d2c739a17afdce7eb711c3
Yann <huyy2b@sina.com>
2019-03-23 11:03:26 +0800

    1

--- oauth2-authorization/src/main/java/com/example/demo/oauth2/authorizationserver/JwtAuthorizationApplication.java
+++ oauth2-authorization/src/main/java/com/example/demo/oauth2/authorizationserver/JwtAuthorizationApplication.java
@@ -2,6 +2,10 @@ package com.example.demo.oauth2.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.provider.client.ClientCredentialsTokenEndpointFilter;
+import org.springframework.security.oauth2.provider.client.ClientDetailsUserDetailsService;
+import org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
 
 @SpringBootApplication
 public class JwtAuthorizationApplication {
@@ -9,5 +13,14 @@ public class JwtAuthorizationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtAuthorizationApplication.class, args);
 	}
+
+	AuthorizationEndpoint authorizationEndpoint;
+
+	TokenEndpoint tokenEndpoint;
+
+	ClientCredentialsTokenEndpointFilter clientCredentialsTokenEndpointFilter;
+
+	ClientDetailsUserDetailsService clientDetailsUserDetailsService;
+
 
 }
--- security-base/src/main/java/com/example/demo/base/Application.java
+++ security-base/src/main/java/com/example/demo/base/Application.java
@@ -2,10 +2,27 @@ package com.example.demo.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+
     }
+
+    SecurityContextHolder context;
+
+    Authentication authentication;
+
+    AuthenticationManager authenticationManager;
+
+    ProviderManager providerManager;
+
+    UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter;
 }
